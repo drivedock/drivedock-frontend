998f19a5fc511de60edf02f3fc2edeb0363c64d4
Shreeshahub <khwaab11@gmail.com>
2023-03-18 22:43:52 +0530

    login and signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
 
 function App() {
 
   return (
-    <HomePage />
+    <LoginPage />
   )
 }
 
