ea654cd8bb303f00b48cb25b6e6970eb46f7050f
khwaab11 <khwaab11@gmail.com>
2023-03-25 15:08:26 +0530

    signup and sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from './assets/react.svg'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
+import UserSignInPage from './pages/UserSignInPage'
+import UserSignUpPage from './pages/UserSignUpPage'
+
+import './index.css'
 
 function App() {
 
   return (
-    <LoginPage />
+    <UserSignUpPage/>
   )
 }
 
