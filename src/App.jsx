d1eb16e6c50fd0b1e545078226a3bbb0b3d1b392
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-05-13 10:22:12 +0530

    added protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,36 @@
 import { Switch, Route } from "react-router-dom";
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import UserSignInPage from './pages/UserSignInPage'
-import UserSignUpPage from './pages/UserSignUpPage'
+import HomePage from "./pages/HomePage";
+import UserSignInPage from "./pages/UserSignInPage";
+import UserSignUpPage from "./pages/UserSignUpPage";
 import Profile from "./pages/Profile";
-import './index.css'
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
-
+import "./index.css";
 
 function App() {
- return (
-  <>
-   <Switch>
-    <Route path="/" exact>
-     <HomePage />
-    </Route>
+  return (
+    <>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
 
-    <Route path="/profile" exact>
-     <Profile />
-    </Route>
+        <Route path="/signin" exact>
+          <UserSignInPage />
+        </Route>
 
-    <Route path="/signin" exact>
-     <UserSignInPage />
-    </Route>
+        <Route path="/signup" exact>
+          <UserSignUpPage />
+        </Route>
 
-    <Route path="/signup" exact>
-     <UserSignUpPage />
-    </Route>
+        <ProtectedRoute path="/profile" exact component={Profile} />
 
-    <Route path="*" exact>
-     {"No Data"}
-    </Route>
-   </Switch>
-  </>
- );
+        <Route path="*" exact>
+          {"No Data"}
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
