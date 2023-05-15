a97b856ec2d1fc7b8736c3b6c386e94d8e6c7243
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-05-15 23:19:38 +0530

    added ec2 api url

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const EMAIL_REGEX =
   /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
 
 export const API_URL =
-  "https://da90-2405-201-c400-8064-b5d1-6093-e314-dcd8.ngrok-free.app/api";
+  "http://ec2-43-205-238-132.ap-south-1.compute.amazonaws.com:3000/api";
