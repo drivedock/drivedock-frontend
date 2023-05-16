939e81f781e6bbebfb63bdf9130a7a4b2f2b7e3e
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-05-16 21:46:24 +0530

    updated the API URL

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const EMAIL_REGEX =
   /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g;
 
 export const API_URL =
-  "http://ec2-43-205-238-132.ap-south-1.compute.amazonaws.com:3000/api";
+  "https://busi8jyxcd.execute-api.ap-south-1.amazonaws.com/prod/api";
