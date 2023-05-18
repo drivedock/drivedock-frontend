1da107ba6017eb2fa9628a94aea0d1171f61ee3e
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-05-18 23:12:46 +0530

    disabled side navbar

--- src/components/InfoBanner/index.jsx
+++ src/components/InfoBanner/index.jsx
@@ -5,19 +5,19 @@ import { InformationCircleIcon, XMarkIcon } from "@heroicons/react/24/outline";
 export const InfoBanner = () => {
   return (
     <>
-      <div className="border-l-4 border-green-200 p-4 rounded-md bg-green-100">
+      <div className="border-l-4 border-green-200 p-2 rounded-md bg-green-100">
         <div className="flex items-center space-x-4">
           <div className="">
             <InformationCircleIcon className="h-6 w-6 text-indigo-600" />
           </div>
-          <div className="">
+          <div className="mt-3">
             <p className="text-sm font-medium text-indigo-600">
-              Register now for upcoming workshop in your college. <a className=" underline text-blue-400" href="">Click here</a>
+              We're still cooking our site, Hold tight!
             </p>
           </div>
-          <div>
+          {/* <div>
             <XMarkIcon className="h-6 w-6 text-indigo-600 cursor-pointer" />
-          </div>
+          </div> */}
         </div>
       </div>
     </>
--- src/pages/SideNavBar/index.jsx
+++ src/pages/SideNavBar/index.jsx
@@ -87,7 +87,7 @@ function SideNavBar(props) {
           <a
             className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
             href="javascript:void(0)"
-            onClick={() => handlePage("workshops")}
+            // onClick={() => handlePage("workshops")}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -155,7 +155,7 @@ function SideNavBar(props) {
           <a
             className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
             href="javascript:void(0)"
-            onClick={() => handlePage("profileSetting")}
+            // onClick={() => handlePage("profileSetting")}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
