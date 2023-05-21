7f9c477b5feb4b4dc0cf2f22d5513219ab69ffef
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-05-21 19:16:35 +0530

    changed nav bar tabs names

--- src/pages/SideNavBar/index.jsx
+++ src/pages/SideNavBar/index.jsx
@@ -10,15 +10,15 @@ import NotePadImg from "../SideNavBar/assets/notepad.png";
 import ChatImg from "../SideNavBar/assets/chat.png";
 import TodoImg from "../SideNavBar/assets/todo.png";
 import ProfileImg from "../SideNavBar/assets/user.png";
-import dummyUserImg from '../SideNavBar/assets/dummy_user_profile_pic.png';
+import dummyUserImg from "../SideNavBar/assets/dummy_user_profile_pic.png";
 import SettingImg from "../SideNavBar/assets/settings.png";
 import SideEndLogoImg from "../SideNavBar/assets/sideEndLogo.png";
 
 function SideNavBar(props) {
   const { handlePage } = props;
-  let userEmail = localStorage.getItem('DDUserEmail');
-  if(userEmail) {
-    userEmail = userEmail.split('@')[0];
+  let userEmail = localStorage.getItem("DDUserEmail");
+  if (userEmail) {
+    userEmail = userEmail.split("@")[0];
   }
 
   const history = useHistory();
@@ -111,57 +111,13 @@ function SideNavBar(props) {
               ></path>
             </svg>
             <span className="mx-2 text-sm font-medium letSdisable">
-              Workshops
+              Meet experts
             </span>
-          </a>
-          <a
-            className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-            href="#"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z"
-              ></path>
-            </svg>
-            <span className="mx-2 text-sm font-medium letSdisable">
-              Projects
-            </span>
-          </a>
-          <a
-            className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
-            href="#"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
-              ></path>
-            </svg>
-            <span className="mx-2 text-sm font-medium letSdisable">Users</span>
           </a>
           <a
             className="flex items-center px-3 py-2 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
             href="javascript:void(0)"
-            // onClick={() => handlePage("profileSetting")}
+            onClick={() => handlePage("profileSetting")}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
