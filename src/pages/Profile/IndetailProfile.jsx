a611a993599da7ef6329f2465441897fcecba0ad
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-05-22 22:26:27 +0530

    adjusted styles for indetail profile

--- src/pages/Profile/IndetailProfile.jsx
+++ src/pages/Profile/IndetailProfile.jsx
@@ -8,7 +8,7 @@ function IndetailProfile() {
   let value = ["Personal Details", "Notification", "Privacy", "Payment"];
   const [page, setPage] = useState("Personal Details");
   return (
-    <section className="indetailProfile flex75">
+    <section className="flex flex-col  h-80 p-5 w-full">
       <div className="">
         {/* <div className="flex30 display__Flex alignCenter indetailTop">
      <h3 style={{ fontSize: "1.5rem" }}>Profile</h3>
