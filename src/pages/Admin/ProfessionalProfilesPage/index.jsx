e5d7c81d2398d75357f9dc110fa59947860b64cd
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-06-03 15:54:43 +0530

    added topics array in create professional

--- src/pages/Admin/ProfessionalProfilesPage/index.jsx
+++ src/pages/Admin/ProfessionalProfilesPage/index.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom";
+import TagsInput from "react-tagsinput";
+
+import "react-tagsinput/react-tagsinput.css";
 import { DEPARTMENTS } from "../../../constants";
+import { createProfessionalProfile } from "../../../api/admin";
 
 function ProfessionalProfilesPage() {
   let history = useHistory();
@@ -10,7 +14,10 @@ function ProfessionalProfilesPage() {
     desc: "",
     location: "",
     dept: "",
+    education: "",
   });
+
+  const [topics, setTopics] = useState([]);
 
   const [errorMsg, setErrorMsg] = useState("");
 
@@ -21,7 +28,7 @@ function ProfessionalProfilesPage() {
     }
     e.preventDefault();
     try {
-      const response = await createWorkshop(inputs);
+      const response = await createProfessionalProfile({ ...inputs, topics });
       if (response.success) {
         history.replace("/admin/workshops");
       } else {
@@ -140,9 +147,6 @@ function ProfessionalProfilesPage() {
                   id="cars"
                   onChange={handleChange}
                 >
-                  <option value="" disabled selected>
-                    Select corresponding department
-                  </option>
                   {DEPARTMENTS.map((dept) => {
                     return (
                       <option key={dept} value={dept}>
@@ -153,6 +157,49 @@ function ProfessionalProfilesPage() {
                 </select>
               </div>
             </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor=""
+                  className="text-base font-medium text-gray-900"
+                >
+                  {" "}
+                  Highest Degree{" "}
+                </label>
+              </div>
+              <div className="mt-2.5">
+                <input
+                  className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                  type="text"
+                  placeholder="Highest degree"
+                  name="education"
+                  onChange={handleChange}
+                  required
+                ></input>
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor=""
+                  className="text-base font-medium text-gray-900"
+                >
+                  {" "}
+                  Topics{" "}
+                </label>
+              </div>
+              <div className="mt-2.5 w-full">
+                <TagsInput
+                  value={topics}
+                  inputProps={{
+                    placeholder: "Add a topic",
+                  }}
+                  onChange={(topics) => setTopics(topics)}
+                />
+              </div>
+            </div>
+
             {errorMsg && (
               <div className="mt-1.5 text-red-400 text-sm">{errorMsg}</div>
             )}
