924b132d9ef3486c44213915749421ae893fc3e6
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-06-11 13:16:25 +0530

    changed the field name

--- src/pages/MeetExperts/ExpertPage.jsx
+++ src/pages/MeetExperts/ExpertPage.jsx
@@ -135,13 +135,14 @@ function ExpertPage() {
                       className="text-base font-medium text-gray-900"
                     >
                       {" "}
-                      Drop your query{" "}
+                      Why you want to interact?{" "}
                     </label>
                   </div>
                   <textarea
                     type="text"
                     className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
                     name="desc"
+                    placeholder="Please brief why you want to interact?"
                     onChange={handleChange}
                     required
                     rows={2}
