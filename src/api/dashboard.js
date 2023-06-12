5934049b52b5fd67bb102587d979efdcfd8da50a
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-06-12 22:55:23 +0530

    added pagination in profile page

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { API_URL } from "../constants";
 
-export const getProfessionalProfiles = async () => {
+export const getProfessionalProfiles = async (offset) => {
   const token = localStorage.getItem("isAuthenticated");
   axios.defaults.headers.common = { token: token };
   const response = await axios.get(
-    `${API_URL}/professionals?offset=${0}&limit=${25}`
+    `${API_URL}/professionals?offset=${offset}&limit=${25}`
   );
   return response.data;
 };
--- src/pages/MeetExperts/index.jsx
+++ src/pages/MeetExperts/index.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  MDBPagination,
+  MDBPaginationItem,
+  MDBPaginationLink,
+} from "mdb-react-ui-kit";
 
 import { getProfessionalProfiles } from "../../api/dashboard";
 import ProfessionalProfileCard from "./ProfessionalProfileCard";
@@ -7,13 +12,20 @@ import ProfessionalProfileCard from "./ProfessionalProfileCard";
 export default function MeetExperts() {
   let mounted = false;
   let history = useHistory();
+  const totalPages = Array.from(Array(5).keys());
   const [professionals, setProfessionals] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  async function fetchData() {
+    const res = await getProfessionalProfiles(currentPage);
+    setProfessionals(res.professionals);
+  }
 
   useEffect(() => {
-    async function fetchData() {
-      const res = await getProfessionalProfiles();
-      setProfessionals(res.professionals);
-    }
+    fetchData();
+  }, [currentPage]);
+
+  useEffect(() => {
     if (!mounted) {
       mounted = true;
       fetchData();
@@ -29,9 +41,13 @@ export default function MeetExperts() {
     });
   };
 
+  const handlePaginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
-      <section>
+      <section className="pb-5">
         <h4>Meet Experts</h4>
         <div className="d-flex mt-4 flex-wrap">
           {professionals.map((pro, index) => {
@@ -43,6 +59,37 @@ export default function MeetExperts() {
             );
           })}
         </div>
+        <nav
+          aria-label="Page navigation example"
+          className="d-flex justify-center"
+        >
+          <MDBPagination className="mb-0">
+            <MDBPaginationItem
+              className="mr-2"
+              onClick={() => handlePaginate(currentPage - 1)}
+              disabled={currentPage === 0}
+            >
+              <MDBPaginationLink>Previous</MDBPaginationLink>
+            </MDBPaginationItem>
+            {totalPages.map((pageNumber) => {
+              return (
+                <MDBPaginationItem
+                  active={pageNumber === currentPage}
+                  onClick={() => handlePaginate(pageNumber)}
+                >
+                  <MDBPaginationLink>{pageNumber + 1}</MDBPaginationLink>
+                </MDBPaginationItem>
+              );
+            })}
+            <MDBPaginationItem
+              className="ml-2"
+              onClick={() => handlePaginate(currentPage + 1)}
+              disabled={totalPages.length - 1 === currentPage}
+            >
+              <MDBPaginationLink>Next</MDBPaginationLink>
+            </MDBPaginationItem>
+          </MDBPagination>
+        </nav>
       </section>
     </div>
   );
