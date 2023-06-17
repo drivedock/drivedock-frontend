1ba4ad23c4b8b64f4c1a458151b6ec3dab2a24bc
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-06-17 16:31:18 +0530

    added redirection for ctas in home page

--- src/pages/HomePage/AllCards/Card.jsx
+++ src/pages/HomePage/AllCards/Card.jsx
@@ -1,5 +1,5 @@
 function Card(props) {
-  const { image, title} = props;
+  const { image, title, handleNavigation, routeName } = props;
   return (
     <>
       <article className="overflow-hidden rounded-md py-4 border">
@@ -7,7 +7,10 @@ function Card(props) {
         <div className="px-4 py-4">
           <div className="font-bold text-xl mb-2 text-center">{title}</div>
         </div>
-        <div className="px-6 pt-2 pb-2">
+        <div
+          className="px-6 pt-2 pb-2 cursor-pointer"
+          onClick={() => handleNavigation(routeName)}
+        >
           <span className="block text-center  bg-gray-200 rounded-full px-3 py-1  text-sm font-semibold text-gray-700 mr-2 mb-2 m-auto">
             Explore
           </span>
--- src/pages/HomePage/AllCards/index.jsx
+++ src/pages/HomePage/AllCards/index.jsx
@@ -6,13 +6,42 @@ import thirdCard from "../../../assets/thirdCard.png";
 import fourchCard from "../../../assets/fourchCard.png";
 
 function AllCards() {
+  const isAuthenticated = localStorage.getItem("isAuthenticated");
+
+  const handleNavigation = (targetPage) => {
+    if (!isAuthenticated) {
+      window.location.href = "/signin";
+    } else {
+      window.location.href = targetPage;
+    }
+  };
   return (
     <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-5 container">
-      <Card image={firstCard} title={'Workshops'} />
-      <Card image={secondCard} title={'Meet Experts'}/>
-      <Card image={thirdCard} title={'Idea Dropbox'}/>
+      <Card
+        image={firstCard}
+        title={"Workshops"}
+        routeName={"/dashboard/workshops"}
+        handleNavigation={handleNavigation}
+      />
+      <Card
+        image={secondCard}
+        title={"Meet Experts"}
+        routeName={"/dashboard/meet-experts"}
+        handleNavigation={handleNavigation}
+      />
+      <Card
+        image={thirdCard}
+        title={"Idea Dropbox"}
+        routeName={"/dashboard/idea-dropbox"}
+        handleNavigation={handleNavigation}
+      />
 
-      <Card image={fourchCard} title={'R & D'}/>
+      <Card
+        image={fourchCard}
+        title={"R & D"}
+        routeName={"/dashboard/r-and-d-projects"}
+        handleNavigation={handleNavigation}
+      />
     </div>
   );
 }
